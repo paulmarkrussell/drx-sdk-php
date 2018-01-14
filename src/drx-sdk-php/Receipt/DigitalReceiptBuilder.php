b090297874dd3a14d4122be2dae46321b7525d20
lajthabalazs <lajthabalazs@yahoo.com>
2018-01-14 09:26:37 +0100

    Added builder.

---
+++ src/drx-sdk-php/Receipt/DigitalReceiptBuilder.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lajtha
+ * Date: 2018-01-14
+ * Time: 09:22
+ */
+
+namespace Dreceiptx\Receipt;
+
+
+class DigitalReceiptBuilder
+{
+    private $receipt;
+
+    public function __construct()
+    {
+    }
+
+    public function build()
+    {
+        return this.receipt;
+    }
+}
