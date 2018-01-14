9dbfd27601a07581e799227c7c65e75f50008eaf
lajthabalazs <lajthabalazs@yahoo.com>
2018-01-14 09:20:58 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# drx-sdk-php
+
+PHP SDK for generating and consuming digital receipts to and from the Digital Receipt Exchange (dRx).
---
+++ src/drx-sdk-php/Receipt/DigitalReceipt.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lajtha
+ * Date: 2018-01-14
+ * Time: 09:16
+ */
+
+namespace Dreceiptx\Receipt;
+
+
+class DigitalReceipt
+{
+    public static function fromJson($json)
+    {
+    }
+}
