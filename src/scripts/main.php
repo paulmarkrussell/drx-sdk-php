44dec7456e02a84bebc8587d17a5d4af2bda091d
lajthabalazs <lajthabalazs@yahoo.com>
2018-01-14 09:46:19 +0100

    Added sample files.

--- src/scripts/main.php
+++ src/scripts/main.php
@@ -6,5 +6,14 @@
  * Time: 09:29
  */
 require_once ("../drx-sdk-php/Receipt/DigitalReceipt.php");
-$receipt = \Dreceiptx\Receipt\DigitalReceipt::fromJson("");
+if (count($argv) == 1) {
+    print "File path of json to parse required.";
+    return;
+} elseif (count($argv) > 2) {
+    print "Only a single parameter supported.";
+    return;
+}
+print "Parsing file ".$argv[1]."\n";
+$json = file_get_contents($argv[1]);
+$receipt = \Dreceiptx\Receipt\DigitalReceipt::fromJson($json);
 print $receipt->toJson();
