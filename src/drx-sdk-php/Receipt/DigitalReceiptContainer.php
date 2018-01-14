f897b0e20649e780b9587e08600aa583212d0c9c
lajthabalazs <lajthabalazs@yahoo.com>
2018-01-14 16:01:06 +0100

    Added types, made class variabled private.

--- src/drx-sdk-php/Receipt/DigitalReceiptContainer.php
+++ src/drx-sdk-php/Receipt/DigitalReceiptContainer.php
@@ -9,11 +9,19 @@
 namespace Dreceiptx\Receipt;
 
 require_once __DIR__.'/../vendor/autoload.php';
-require_once __DIR__.'/DigitalReceipt.php';
+require_once __DIR__ . '/DRxDigitalReceipt.php';
 
-class DigitalReceiptContainer
+class DigitalReceiptContainer implements \JsonSerializable
 {
-    public $dRxDigitalReceipt;
+    private $dRxDigitalReceipt;
+
+    /**
+    * @param mixed $dRxDigitalReceipt
+    */
+    public function setDRxDigitalReceipt(DRxDigitalReceipt $dRxDigitalReceipt)
+    {
+        $this->dRxDigitalReceipt = $dRxDigitalReceipt;
+    }
 
     public static function fromJson($json)
     {
@@ -25,5 +33,19 @@ class DigitalReceiptContainer
     public function toJson()
     {
         return json_encode($this);
+    }
+
+    /**
+     * Specify data which should be serialized to JSON
+     * @link http://php.net/manual/en/jsonserializable.jsonserialize.php
+     * @return mixed data which can be serialized by <b>json_encode</b>,
+     * which is a value of any type other than a resource.
+     * @since 5.4.0
+     */
+    public function jsonSerialize()
+    {
+        $ret = new \stdClass();
+        $ret->dRxDigitalReceipt = $this->dRxDigitalReceipt;
+        return $ret;
     }
 }
