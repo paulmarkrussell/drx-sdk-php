dad143b5f7b3b968d94aef131137c632578ba3db
lajthabalazs <lajthabalazs@yahoo.com>
2018-02-10 07:36:18 +0100

    Created empty test.

---
+++ src/drx-sdk-php/tests/Receipt/ReceiltTotalTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lajtha
+ * Date: 2018-02-10
+ * Time: 07:34
+ */
+
+class ReceiltTotalTest extends \PHPUnit\Framework\TestCase
+{
+    public function testEmptyReceiptHasZeroTotal() {
+
+        $this->assertEquals(0, 0);
+    }
+}
